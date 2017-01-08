a96b4e7e74ac86c93630cc76f0a0527c7bd0e1cd
sherman5 <tomsherman159@gmail.com>
2017-01-08 17:05:13 -0500

    test suite working

--- tests/testing.h
+++ tests/testing.h
@@ -3,15 +3,16 @@
 
 #include <stdint.h>
 
-uint32_t* outputAddress;
+extern uint32_t* outputAddress;
 
 #define REQUIRE(x) \
     do { \
         if (!(x)) { \
-            *(outputAddress++) = 0xFFFF0000; \
-        } else if (*(outputAddress) != 0xFFFF0000) { \
-            *(outputAddress++) = 0x0000AAAA; \
-        } \
+            *outputAddress = 0xFFFF0000; \
+        } else if (*outputAddress != 0xFFFF0000) { \
+            *outputAddress = 0x0000AAAA; \
+        }\
+        outputAddress++;\
     } while (0)
 
 #define APPROX_EQ(a,b,tol) ((a) < (b) ? (b) - (a) < tol : (a) - (b) < tol)
