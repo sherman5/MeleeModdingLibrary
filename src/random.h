135cb88b7f7fc0706a1150b3c4491d575438016a
sherman5 <tomsherman159@gmail.com>
2017-01-12 11:23:08 -0500

    added sample to random.h

--- src/random.h
+++ src/random.h
@@ -17,17 +17,26 @@
 uint32_t randUint32();
 
 /**
- * @brief Generate random number in [0,1]
+ * @brief Generate random number in [0,1].
  *
- * @return random floating point number in [0,1]
+ * @return - Random floating point number in [0,1]
  */
 float rand();
 
 /**
- * @brief Sample uniformly from [@p a,@p b]
+ * @brief Sample uniformly from [@p a,@p b].
  *
  * @return random floating point number in [@p a, @p b]
  */
 float uniform(float a, float b);
+
+/**
+ * @brief Return index of array according to given probabilities.
+ *
+ * @param probs[] - array of probabilities, must sum to 1
+ *
+ * @return index of array
+ */
+unsigned sample(float probs[]);
 
 #endif
