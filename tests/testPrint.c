0d2e1dad9fb155797bbd38bad7c3c88f08b55721
Tom Sherman <tsherma4@jhu.edu>
2017-03-07 04:35:07 -0500

    restore test print

--- tests/testPrint.c
+++ tests/testPrint.c
@@ -7,7 +7,7 @@
 
 static char heap[15000];
 static bool init = false;
-//static char str[100];
+static char str[100];
 
 void _main()
 {
@@ -15,9 +15,7 @@ void _main()
 
         initHeap(heap, heap + sizeof(heap));
 
-print("One of the coolest features of this library is the ability to print text in the game. This allows for error reporting, unit testing, and more tools for debugging your code. Perhaps the most important thing is that debugging tools are now available on the Wii/GameCube. Previously, all of the debugging had to be done in Dolphin, and if your mod didn't work in the Wii is was all up to guesswork to diagnose the problem.");
-
-/*        memset(str, 0, sizeof(str));
+        memset(str, 0, sizeof(str));
         strcat(str, "this is a test of how strings are wrapped when");
         strcat(str, " they are too long for a single line");
 
@@ -51,7 +49,7 @@ print("One of the coolest features of this library is the ability to print text 
         print("line 29");
         print("line 30");
         print("line 31");
-        THROW_ERROR("test error taking up multiple lines of output blah blah");
+/*        THROW_ERROR("test error taking up multiple lines of output blah blah");
         print("line 31");*/
         init = true;
     }
