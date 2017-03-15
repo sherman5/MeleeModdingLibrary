39d9b158d643cfac6c523729814f7e0f8b09b718
Tom Sherman <tsherma4@jhu.edu>
2017-03-15 11:25:33 -0400

    fixed bug in calloc

--- src/system.c
+++ src/system.c
@@ -39,6 +39,7 @@ void* malloc(size_t size)
 void* calloc(size_t num, size_t size)
 {
     void* ptr = malloc(num * size);
+    if (!ptr) {return ptr;}
     memset(ptr, 0, num * size);
     return ptr;
 }
